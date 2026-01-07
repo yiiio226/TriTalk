55d44c81984956d33e912e799641c0572172c81a
cyb <allen64326432@gmail.com>
2026-01-07 10:27:28 +0800

    update

--- frontend/lib/env.example.dart
+++
@@ -1,6 +0,0 @@
-class Env {
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL_HERE';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY_HERE';
-  static const String localBackendUrl = 'http://192.168.1.4:8787';
-  static const String prodBackendUrl = 'https://tritalk-backend.tristart226.workers.dev';
-}
