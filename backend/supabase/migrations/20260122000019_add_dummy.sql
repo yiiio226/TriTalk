139d842865d040301ff76b2f50eb13e8e6301500
cyb <allen64326432@gmail.com>
2026-01-21 10:45:47 +0800

    update

--- backend/supabase/migrations/20260122000019_add_dummy.sql
+++ backend/supabase/migrations/20260122000019_add_dummy.sql
@@ -1,4 +1,4 @@
 create table if not exists dummy (
   id uuid primary key, -- client-generated UUID  
-  created_at timestamp with time zone default now(),
+  created_at timestamp with time zone default now()
 );
